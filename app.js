19db1cf225f4c6757d6887ab0047876953a9b402
sreeragp07 <84373517+sreeragp07@users.noreply.github.com>
2023-08-27 22:20:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// var num = Math.floor(Math.random() * 4) + 1;
+
+$("#card").wScratchPad({
+  size: 70, // The size of the brush/scratch.
+  // bg: `Images/front.jpg`, // Background (image path or hex color).
+  fg: `Images/google-scratch-card.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
